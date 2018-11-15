49163b084f8030f09c9cccfd1826538639e879f9
Afonia27 <36937082+Afonia27@users.noreply.github.com>
2018-11-15 01:43:17 +0000

    Add files via upload

---
+++ Application.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 20:22:32 2018
+
+@author: AfanasiChihaioglo
+"""
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5 import QtCore, QtGui, QtWidgets
+      # Include the GUI.py
+# -*- coding: utf-8 -*-
+
+def func():
+         print("Clicked")
+
+class Ui_MainWindow(object):         # All of these will be imported in header
+    
+    def openFile(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(None,"QFileDialog.getOpenFileName()", "","All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            self.list.append(fileName)
+            file = open(fileName,"r")
+            self.list.append(file.read())
+            file.close()
+            return fileName
+    
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(754, 436)
+        MainWindow.setWhatsThis("")
+        MainWindow.setAutoFillBackground(False)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(40, 120, 125, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(40, 160, 113, 32))
+        self.pushButton.setObjectName("pushButton")
+        self.list = QtWidgets.QTextBrowser(self.centralwidget)
+        self.list.setGeometry(QtCore.QRect(220, 30, 131, 291))
+        self.list.setObjectName("list")
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(410, 30, 256, 192))
+        self.graphicsView.setObjectName("graphicsView")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(510, 10, 60, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(270, 10, 60, 16))
+        self.label_2.setObjectName("label_2")
+        self.textBrowser_2 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setGeometry(QtCore.QRect(410, 270, 41, 21))
+        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.textBrowser_3 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_3.setGeometry(QtCore.QRect(500, 270, 41, 21))
+        self.textBrowser_3.setObjectName("textBrowser_3")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(400, 250, 60, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(500, 250, 60, 16))
+        self.label_4.setObjectName("label_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 754, 22))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(self.openFile)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Program"))
+        self.pushButton.setText(_translate("MainWindow", "Load file"))
+        self.label.setText(_translate("MainWindow", "Graph"))
+        self.label_2.setText(_translate("MainWindow", "List"))
+        self.label_3.setText(_translate("MainWindow", "Gradient"))
+        self.label_4.setText(_translate("MainWindow", "R^2"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.statusbar.setStatusTip(_translate("MainWindow", "hh"))
+
+class AppWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.show()  
+
+app = QApplication(sys.argv)
+w = AppWindow()
+w.show()
+sys.exit(app.exec_())
